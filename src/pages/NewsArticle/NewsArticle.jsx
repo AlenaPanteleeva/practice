c1397b7e4c6a1527cf7eac64ae0b2df91dc6a40f
Alena Panteleeva <pantalena7773@gmail.com>
2025-06-10 23:37:07 +0700

    upd README / host 0 0 0 0

--- src/pages/NewsArticle/NewsArticle.jsx
+++ src/pages/NewsArticle/NewsArticle.jsx
@@ -9,7 +9,7 @@ const NewsArticle = () => {
   return (
     <>
       <header className='bg-white shadow'>
-        <div className='mx-auto max-w-7xl px-2 py-6 sm:px-6 lg:px-8'>
+        <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
         </div>
       </header>
       <main>
